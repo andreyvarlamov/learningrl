4f3837c6474348f0af77ebaa9c4d5bc7879cf42e
andreyvarlamov <35208582+andreyvarlamov@users.noreply.github.com>
2024-02-01 17:09:07 -0800

    3 - 2D Camera

--- source/learningrl.cpp
+++ source/learningrl.cpp
@@ -14,6 +14,8 @@ enum GameScreen
     ENDING
 };
 
+#define MAX_BUILDINGS 100
+
 int main(int argv, char **argc)
 {
     int screenWidth = 800;
@@ -21,88 +23,87 @@ int main(int argv, char **argc)
 
     InitWindow(screenWidth, screenHeight, "Learning");
 
-    GameScreen currentScreen = LOGO;
+    Rectangle player = { 400, 280, 40, 40 };
+    Rectangle buildings[MAX_BUILDINGS] = { 0 };
+    Color buildColors[MAX_BUILDINGS] = { 0 };
 
-    int framesCounter = 0;
+    int spacing =  0;
+
+    for (int i = 0; i < MAX_BUILDINGS; i++)
+    {
+        buildings[i].width = (f32) GetRandomValue(50, 200);
+        buildings[i].height = (f32) GetRandomValue(100, 800);
+        buildings[i].y = screenHeight - 130.0f - buildings[i].height;
+        buildings[i].x = -6000.0f + spacing;
+
+        spacing += (int) buildings[i].width;
+        buildColors[i] = { (u8) GetRandomValue(200, 240), (u8) GetRandomValue(200, 240), (u8) GetRandomValue(200, 250), 255 };
+    }
+
+    Camera2D camera = { 0 };
+    camera.target = { player.x + 20.0f, player.y + 20.0f };
+    camera.offset = { screenWidth / 2.0f, screenHeight / 2.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
-        switch (currentScreen)
+        if (IsKeyDown(KEY_RIGHT)) player.x += 2;
+        else if (IsKeyDown(KEY_LEFT)) player.x -= 2;
+
+        camera.target = { player.x + 20.0f, player.y + 20.0f };
+
+        if (IsKeyDown(KEY_A)) camera.rotation--;
+        else if (IsKeyDown(KEY_S)) camera.rotation++;
+
+        if (camera.rotation > 40) camera.rotation = 40;
+        else if (camera.rotation < -40) camera.rotation = -40;
+
+        camera.zoom += ((f32) GetMouseWheelMove() * 0.05f);
+
+        if (camera.zoom > 3.0f) camera.zoom = 3.0f;
+        else if (camera.zoom < 0.3f) camera.zoom = 0.1f;
+
+        if (IsKeyPressed(KEY_R))
         {
-            case LOGO:
-            {
-                framesCounter++;
-
-                if (framesCounter > 120)
-                {
-                    currentScreen = TITLE;
-                }
-            } break;
-
-            case TITLE:
-            {
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-                {
-                    currentScreen = GAMEPLAY;
-                }
-            } break;
-
-            case GAMEPLAY:
-            {
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-                {
-                    currentScreen = ENDING;
-                }
-            } break;
-
-            case ENDING:
-            {
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-                {
-                    currentScreen = TITLE;
-                }
-            } break;
-
-            default: break;
+            camera.zoom = 1.0f;
+            camera.rotation = 0.0f;
         }
 
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
-            switch (currentScreen)
-            {
-                case LOGO:
-                {
-                    DrawText("Logo screen", 20, 20, 40, LIGHTGRAY);
-                    DrawText("Wait for 2 seconds", 290, 220, 20, GRAY);
-                } break;
+            BeginMode2D(camera);
 
-                case TITLE:
-                {
-                    DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
-                    DrawText("Title screen", 20, 20, 40, DARKGREEN);
-                    DrawText("Press ENTER or TAP to jump to gameplay screen", 120, 220, 20, DARKGREEN);
-                } break;
+                DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY);
 
-                case GAMEPLAY:
-                {
-                    DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
-                    DrawText("Gameplay screen", 20, 20, 40, MAROON);
-                    DrawText("Press ENTER or TAP to jump to ending screen", 120, 220, 20, MAROON);
-                } break;
+                for (int i = 0; i < MAX_BUILDINGS; i++) DrawRectangleRec(buildings[i], buildColors[i]);
 
-                case ENDING:
-                {
-                    DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
-                    DrawText("Ending screen", 20, 20, 40, DARKBLUE);
-                    DrawText("Press ENTER or TAP to return to title screen", 120, 220, 20, DARKBLUE);
-                } break;
+                DrawRectangleRec(player, RED);
 
-                default: break;
-            }
+                DrawLine((int) camera.target.x, -screenHeight * 10, (int) camera.target.x, screenHeight * 10, GREEN);
+                DrawLine(-screenWidth * 10, (int) camera.target.x, screenWidth * 10, (int) camera.target.y, GREEN);
+
+            EndMode2D();
+
+            DrawText("SCREEN AREA", 640, 10, 20, RED);
+
+            DrawRectangle(0, 0, screenWidth, 5, RED);
+            DrawRectangle(0, 5, 5, screenHeight - 10, RED);
+            DrawRectangle(screenWidth - 5, 5, 5, screenHeight - 10, RED);
+            DrawRectangle(0, screenHeight - 5, screenWidth, 5, RED);
+
+            DrawRectangle(10, 10, 250, 113, Fade(SKYBLUE, 0.5f));
+            DrawRectangleLines(10, 10, 250, 113, BLUE);
+
+            DrawText("Free 2d camera controls:", 20, 20, 10, BLACK);
+            DrawText("- Right/left to move Offset", 40, 40, 10, DARKGRAY);
+            DrawText("- Mouse Wheel to Zoom in-out", 40, 60, 10, DARKGRAY);
+            DrawText("- A/S to Rotate", 40, 80, 10, DARKGRAY);
+            DrawText("- R to reset Zoom and Rotation", 40, 100, 10, DARKGRAY);
 
         EndDrawing();
     }
