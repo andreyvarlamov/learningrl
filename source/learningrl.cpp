4479674a4dbdee8180eec7b1fcf5e8e4c2a35019
andreyvarlamov <35208582+andreyvarlamov@users.noreply.github.com>
2024-02-01 17:25:11 -0800

    4 - 2D Camera Mouse Zoom

--- source/learningrl.cpp
+++ source/learningrl.cpp
@@ -1,20 +1,10 @@
 #include <cstdio>
 
 #include <raylib/raylib.h>
+#include <raylib/rlgl.h>
+#include <raylib/raymath.h>
 
 #include <varand/varand_types.h>
-
-#include "learningrl.h"
-
-enum GameScreen
-{
-    LOGO = 0,
-    TITLE,
-    GAMEPLAY,
-    ENDING
-};
-
-#define MAX_BUILDINGS 100
 
 int main(int argv, char **argc)
 {
@@ -23,87 +13,53 @@ int main(int argv, char **argc)
 
     InitWindow(screenWidth, screenHeight, "Learning");
 
-    Rectangle player = { 400, 280, 40, 40 };
-    Rectangle buildings[MAX_BUILDINGS] = { 0 };
-    Color buildColors[MAX_BUILDINGS] = { 0 };
-
-    int spacing =  0;
-
-    for (int i = 0; i < MAX_BUILDINGS; i++)
-    {
-        buildings[i].width = (f32) GetRandomValue(50, 200);
-        buildings[i].height = (f32) GetRandomValue(100, 800);
-        buildings[i].y = screenHeight - 130.0f - buildings[i].height;
-        buildings[i].x = -6000.0f + spacing;
-
-        spacing += (int) buildings[i].width;
-        buildColors[i] = { (u8) GetRandomValue(200, 240), (u8) GetRandomValue(200, 240), (u8) GetRandomValue(200, 250), 255 };
-    }
-
-    Camera2D camera = { 0 };
-    camera.target = { player.x + 20.0f, player.y + 20.0f };
-    camera.offset = { screenWidth / 2.0f, screenHeight / 2.0f };
-    camera.rotation = 0.0f;
+    Camera2D camera = {};
     camera.zoom = 1.0f;
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
-        if (IsKeyDown(KEY_RIGHT)) player.x += 2;
-        else if (IsKeyDown(KEY_LEFT)) player.x -= 2;
-
-        camera.target = { player.x + 20.0f, player.y + 20.0f };
-
-        if (IsKeyDown(KEY_A)) camera.rotation--;
-        else if (IsKeyDown(KEY_S)) camera.rotation++;
-
-        if (camera.rotation > 40) camera.rotation = 40;
-        else if (camera.rotation < -40) camera.rotation = -40;
-
-        camera.zoom += ((f32) GetMouseWheelMove() * 0.05f);
-
-        if (camera.zoom > 3.0f) camera.zoom = 3.0f;
-        else if (camera.zoom < 0.3f) camera.zoom = 0.1f;
-
-        if (IsKeyPressed(KEY_R))
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
         {
-            camera.zoom = 1.0f;
-            camera.rotation = 0.0f;
+            Vector2 delta = GetMouseDelta();
+            delta = Vector2Scale(delta, -1.0f / camera.zoom);
+
+            camera.target = Vector2Add(camera.target, delta);
+        }
+
+        float wheel = GetMouseWheelMove();
+
+        if (wheel != 0)
+        {
+            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+
+            camera.offset = GetMousePosition();
+
+            camera.target = mouseWorldPos;
+
+            f32 zoomIncrement = 0.125f;
+
+            camera.zoom += (wheel * zoomIncrement);
+            if (camera.zoom < zoomIncrement) camera.zoom = zoomIncrement;
         }
 
         BeginDrawing();
-
-            ClearBackground(RAYWHITE);
+            ClearBackground(BLACK);
 
             BeginMode2D(camera);
 
-                DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY);
+                rlPushMatrix();
+                    rlTranslatef(0, 25*50, 0);
+                    rlRotatef(90, 1, 0, 0);
+                    DrawGrid(100, 50);
+                rlPopMatrix();
 
-                for (int i = 0; i < MAX_BUILDINGS; i++) DrawRectangleRec(buildings[i], buildColors[i]);
-
-                DrawRectangleRec(player, RED);
-
-                DrawLine((int) camera.target.x, -screenHeight * 10, (int) camera.target.x, screenHeight * 10, GREEN);
-                DrawLine(-screenWidth * 10, (int) camera.target.x, screenWidth * 10, (int) camera.target.y, GREEN);
+                DrawCircle(100, 100, 50, YELLOW);
 
             EndMode2D();
 
-            DrawText("SCREEN AREA", 640, 10, 20, RED);
-
-            DrawRectangle(0, 0, screenWidth, 5, RED);
-            DrawRectangle(0, 5, 5, screenHeight - 10, RED);
-            DrawRectangle(screenWidth - 5, 5, 5, screenHeight - 10, RED);
-            DrawRectangle(0, screenHeight - 5, screenWidth, 5, RED);
-
-            DrawRectangle(10, 10, 250, 113, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines(10, 10, 250, 113, BLUE);
-
-            DrawText("Free 2d camera controls:", 20, 20, 10, BLACK);
-            DrawText("- Right/left to move Offset", 40, 40, 10, DARKGRAY);
-            DrawText("- Mouse Wheel to Zoom in-out", 40, 60, 10, DARKGRAY);
-            DrawText("- A/S to Rotate", 40, 80, 10, DARKGRAY);
-            DrawText("- R to reset Zoom and Rotation", 40, 100, 10, DARKGRAY);
+            DrawText("Mouse right button drag to move, mouse wheel to zoom", 10, 10, 20, WHITE);
 
         EndDrawing();
     }
