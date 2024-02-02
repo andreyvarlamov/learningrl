e5d7026fcb9707ae9b8719417467f34c62381622
andreyvarlamov <35208582+andreyvarlamov@users.noreply.github.com>
2024-02-01 23:59:26 -0800

    13 - Drop Files

--- source/learningrl.cpp
+++ source/learningrl.cpp
@@ -5,50 +5,72 @@
 #include <raylib/raylib.h>
 
 #include <cstdio>
-#include <ctime>
+#include <cstdlib>
 
-void CustomLog(int msgType, const char *text, va_list args)
-{
-    char timeStr[64] = { 0 };
-    time_t now = time(NULL);
-    struct tm *tm_info = localtime(&now);
-
-    strftime(timeStr, sizeof(timeStr), "%Y-%m-%d %H:%M:%S", tm_info);
-    printf("[%s] ", timeStr);
-
-    switch (msgType)
-    {
-        case LOG_INFO: printf("[INFO] : "); break;
-        case LOG_ERROR: printf("[ERROR]: "); break;
-        case LOG_WARNING: printf("[WARN] : "); break;
-        case LOG_DEBUG: printf("[DEBUG]: "); break;
-        default: break;
-    }
-
-    vprintf(text, args);
-    printf("\n");
-}
+#define MAX_FILEPATH_RECORDED   4096
+#define MAX_FILEPATH_SIZE       2048
 
 int main(int argv, char **argc)
 {
     int screenWidth = 800;
     int screenHeight = 600;
 
-    SetTraceLogCallback(CustomLog);
-
     InitWindow(screenWidth, screenHeight, "Learning");
+
+    int filePathCounter = 0;
+    char *filePaths[MAX_FILEPATH_RECORDED] = { 0 };
+
+    for (int i = 0; i < MAX_FILEPATH_RECORDED; i++)
+    {
+        filePaths[i] = (char *)RL_CALLOC(MAX_FILEPATH_SIZE, 1);
+    }
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+        if (IsFileDropped())
+        {
+            FilePathList droppedFiles = LoadDroppedFiles();
+
+            for (int i = 0, offset = filePathCounter; i < (int) droppedFiles.count; i++)
+            {
+                if (filePathCounter , (MAX_FILEPATH_RECORDED - 1))
+                {
+                    TextCopy(filePaths[offset + i], droppedFiles.paths[i]);
+                    filePathCounter++;
+                }
+            }
+
+            UnloadDroppedFiles(droppedFiles);
+        }
+
         BeginDrawing();
+            ClearBackground(RAYWHITE);
 
-        ClearBackground(RAYWHITE);
+            if (filePathCounter == 0) DrawText("Drop your files to this window!", 100, 40, 20, DARKGRAY);
+            else
+            {
+                DrawText("Dropped files: ", 100, 40, 20, DARKGRAY);
 
-        DrawText("Check out the console output to see the custom logger in action!", 60, 200, 20, LIGHTGRAY);
+                for (int i = 0; i < filePathCounter; i++)
+                {
+                    if (i % 2 == 0) DrawRectangle(0, 85 + 40 * i, screenWidth, 40, Fade(LIGHTGRAY, 0.5f));
+                    else DrawRectangle(0, 85 + 40 * i, screenWidth, 40, Fade(LIGHTGRAY, 0.3f));
+
+                    DrawText(filePaths[i], 120, 100 + 40 * i, 10, GRAY);
+                }
+
+                DrawText("Drop new files...", 100, 110 + 40 * filePathCounter, 20, DARKGRAY);
+            }
+
 
         EndDrawing();
+    }
+
+    for (int i = 0; i < MAX_FILEPATH_RECORDED; i++)
+    {
+        RL_FREE(filePaths[i]);
     }
 
     CloseWindow();
